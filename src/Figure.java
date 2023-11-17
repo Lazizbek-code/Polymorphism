1db1946fe62e135515bff83030ebf7c78c24ed3d
Lazizbek-code <ishonch.lazizbek.com>
2023-11-17 17:37:34 +0500

    Overloading

---
+++ src/Figure.java
@@ -0,0 +1,26 @@
+public class Figure {
+    // overloading
+    public int calculation(int a, int b){
+        return 2 * (a+b);
+    }
+
+    public int calculation(int a){
+        return 4 * a;
+    }
+
+    public int calculation(int a, int b, int c){
+        return a+b+c;
+    }
+
+    public int calculation(int a, int b, int c, int d, int f){
+        return a+b+c+d+f;
+    }
+
+    public static void main(String[] args) {
+        Figure figure = new Figure();
+        System.out.println("To'g'ri to'rtburchak perimetri = " + figure.calculation(2,3));
+        System.out.println("Uchburchak perimetri = " + figure.calculation(4,3, 5));
+        System.out.println("Kvadrat perimetri = " + figure.calculation(4));
+        System.out.println("BeshBurchak perimetri = " + figure.calculation(4, 5, 6, 7, 8));
+    }
+}
